b5db11c1ca4cf300317559b1e0d2d37f065f9765
Mike Hacker <mhackersu@gmail.com>
2024-04-23 15:17:29 -0500

    update meta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'mvskokvlke dot com',
+  author: 'Mike Hacker',
+  headerTitle: 'An indigenous take on all things cyber',
+  description: 'Improving indigenous outcomes',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
