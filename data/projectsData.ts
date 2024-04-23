b63dcd87230750a673da74df197ccf9802fccc2d
Mike Hacker <mhackersu@gmail.com>
2024-04-23 16:31:11 -0500

    update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,24 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'BFSIO',
+    description: `A personal project where I am collecting various JS programs such as calculator, 
+    calendar, task list, and weather app, into a collection of exercises for programming curriculum.`,
+    imgSrc: '/static/images/bfsio.png',
+    href: 'https://bfsio.net/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Foster CS',
+    description: `Computer science curriculum for state-sponsored young-adults in foster care to
+    have access to learn the basics of computer science and programming.`,
+    imgSrc: '/static/images/foster-cs.png',
+    href: 'https://fostercs.wpengine.com/',
   },
 ]
 
