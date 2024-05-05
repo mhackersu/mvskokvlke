2a5c8a2a1f19df5d1d1e44a5e2fbc2eeff2ee835
Mike Hacker <mhackersu@gmail.com>
2024-05-04 22:55:35 -0500

    prep blog

---
+++ sample-data/data/projectsData.ts
@@ -0,0 +1,25 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'BFSIO',
+    description: `A personal project where I am collecting various JS programs such as calculator, 
+    calendar, task list, and weather app, into a collection of exercises for programming curriculum.`,
+    imgSrc: '/static/images/bfsio.png',
+    href: 'https://bfsio.net/',
+  },
+  {
+    title: 'Foster CS',
+    description: `Computer science curriculum for state-sponsored young-adults in foster care to
+    have access to learn the basics of computer science and programming.`,
+    imgSrc: '/static/images/foster-cs.png',
+    href: 'https://fostercs.wpengine.com/',
+  },
+]
+
+export default projectsData
