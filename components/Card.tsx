f5e2c7edc9752b66473eb0099b3b1e01b217aac3
Mike Hacker <mhackersu@gmail.com>
2025-06-29 21:06:09 -0500

    projects formatting

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,8 +15,8 @@ const Card = ({ title, description, imgSrc, href }) => (
               alt={title}
               src={imgSrc}
               className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
+              width={200}
+              height={200}
             />
           </Link>
         ) : (
@@ -24,8 +24,8 @@ const Card = ({ title, description, imgSrc, href }) => (
             alt={title}
             src={imgSrc}
             className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
+            width={200}
+            height={200}
           />
         ))}
       <div className="p-6">
